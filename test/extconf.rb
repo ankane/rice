467d71404c0e0349954337ef8ed4ccff7cfea023
Andrew Kane <andrew@ankane.org>
2024-10-13 13:16:28 -0700

    Updated CI

--- test/extconf.rb
+++ test/extconf.rb
@@ -15,7 +15,7 @@ create_makefile(target_exe) do |conf|
   if IS_MSWIN
     conf << "\t$(Q) $(CXX) -Fe$(@) $(OBJS) $(LIBS) $(LOCAL_LIBS) -link $(ldflags) $(LIBPATH)"
   else
-    conf << "\t$(Q) $(CXX) -o $@ $(OBJS) $(LIBPATH) $(LOCAL_LIBS) $(LIBS)"
+    conf << "\t$(Q) $(CXX) -o $@ $(OBJS) $(LIBPATH) $(LOCAL_LIBS) $(LIBS) -l$(RUBY_SO_NAME)"
   end
 
   conf << "\n"
